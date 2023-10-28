034c80e58b1b48f14a80ad1e9130044e0fcf734b
Hitesh Rana“git config --global user.email hitesh79rana@gmail.comgit config --global user.name Hitesh <hitesh79rana@gmail.com>
2023-10-28 15:09:24 +0100

    modify file

--- ByteData.java
+++ ByteData.java
@@ -3,7 +3,7 @@ package mypackage;
 public class ByteData {
     public static void main(String[] args) {
         byte a= -80;
-        byte b= 100;
+        byte b= 120;
         byte c;
         System.out.println("c"+(a+b));
         System.out.println("c"+(a-b));
--- arithmetic.java
+++ arithmetic.java
@@ -3,7 +3,7 @@ package mypackage;
 public class arithmetic {
     public static void main(String[] args) {
         byte a=+50;
-        byte b=-20;
+        byte b=-30;
         byte c;
         System.out.println("c="+(a+b));
         System.out.println("c="+(a-b));
